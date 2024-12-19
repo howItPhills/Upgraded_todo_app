a2607ef721368fa9fb188aba0e60aba3a671ccc9
Uladzislau <uladzislauhr@gmail.com>
2024-12-19 16:27:41 +0300

    switched from sqlite to prisma in all routers

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,59 +1,65 @@
 import express from "express";
 import bcrypt from "bcryptjs";
-import db from "../db.js";
 import jwt from "jsonwebtoken";
+import prismaDb from "../prismaClient.js";
 
 const router = express.Router();
 
-router.post("/login", (req, res) => {
-  const { username, password } = req.body;
+router.post("/login", async (req, res) => {
+  try {
+    const user = await prismaDb.user.findUnique({
+      where: {
+        username: req.body.username,
+      },
+    });
 
-  // Preparing a sql command (? - is for placeholding)
-  const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
-  // This script will inject username to previous script (instead of ?) and get target user
-  const user = getUser.get(username);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+    //Compares the password from request and db user password (hashed/encoded string)
+    const isPasswordValid = bcrypt.compareSync(
+      req.body.password,
+      user.password
+    );
 
-  if (!user) {
-    res.status(404).json({ message: "User not found" });
-    return;
+    if (!isPasswordValid) {
+      res.status(401).json({ message: "Password is invalid" });
+      return;
+    }
+    // If everything is ok - send back a token
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
   }
-
-  //Compares the password from request and db user password (hashed/encoded string)
-  const isPasswordValid = bcrypt.compareSync(password, user.password);
-
-  if (!isPasswordValid) {
-    res.status(401).json({ message: "Password is invalid" });
-    return;
-  }
-  console.log(user);
-
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-    expiresIn: "24h",
-  });
-  res.json({ token });
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const body = req.body;
 
   //Encrypting the password, can't be decrypted
   const hashedPassword = bcrypt.hashSync(body.password, 8);
 
   try {
-    const insertToUsers = db.prepare(`
-      INSERT INTO users (username, password)
-      VALUES (?, ?)
-      `);
-
-    const resultUsers = insertToUsers.run(body.username, hashedPassword);
+    const user = await prismaDb.user.create({
+      data: {
+        username: body.username,
+        password: hashedPassword,
+      },
+    });
 
     const defaultTask = "Hi, let's add your first todo";
-    const insertToTodos = db.prepare(`
-      INSERT INTO todos (user_id, task)
-      VALUES (?, ?)
-      `);
 
-    insertToTodos.run(resultUsers.lastInsertRowid, defaultTask);
+    await prismaDb.todo.create({
+      data: {
+        task: defaultTask,
+        userId: user.id,
+      },
+    });
 
     const token = jwt.sign(
       { id: resultUsers.lastInsertRowid },
@@ -64,7 +70,7 @@ router.post("/register", (req, res) => {
     res.json({ token });
   } catch (error) {
     console.log(error.message);
-    res.sendStatus(503);
+    res.sendStatus(500);
   }
 });
 
--- src/routes/todosRoutes.js
+++ src/routes/todosRoutes.js
@@ -1,56 +1,73 @@
 import express from "express";
-import db from "../db.js";
+import prismaDb from "../prismaClient.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  const getTodos = db.prepare("SELECT * FROM todos WHERE user_id = ?");
-  const todos = getTodos.all(req.userId);
-
-  res.status(200).json(todos);
+router.get("/", async (req, res) => {
+  try {
+    const todos = await prismaDb.todo.findMany({
+      where: {
+        userId: req.userId,
+      },
+    });
+    res.status(200).json(todos);
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(500);
+  }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const userId = req.userId;
   const task = req.body.task;
 
   try {
-    const postTodo = db.prepare(
-      "INSERT INTO todos (user_id, task) VALUES (?, ?)"
-    );
-    postTodo.run(userId, task);
-    res.sendStatus(201);
+    const newTask = await prismaDb.todo.create({
+      data: {
+        task,
+        userId,
+      },
+    });
+    res.status(201).json(newTask);
   } catch (error) {
     console.log(error.message);
     res.sendStatus(500);
   }
 });
 
-router.put("/:todoId", (req, res) => {
+router.put("/:todoId", async (req, res) => {
   const todoId = req.params.todoId;
   const { completed } = req.body;
-
+  // Обязательно проверить и айди пользователя, чтобы не удалить туду с таким же айди другого пользователя
   try {
-    const updateTodo = db.prepare(
-      "UPDATE todos SET completed = ? WHERE id = ?"
-    );
-    updateTodo.run(completed, todoId);
-    res.sendStatus(201);
+    const updated = await prismaDb.todo.update({
+      where: {
+        id: todoId,
+        userId: req.userId,
+      },
+      data: {
+        completed,
+      },
+    });
+    res.status(201).json(updated);
   } catch (error) {
     console.log(error.message);
     res.sendStatus(500);
   }
 });
 
-router.delete("/:todoId", (req, res) => {
+router.delete("/:todoId", async (req, res) => {
   const todoId = req.params.todoId;
   const userId = req.userId;
+
   try {
     // Проверяем id тудушки вместе с id пользователя, чтобы не удалить из базы туду с таким же id другого пользователя
-    const deleteTodo = db.prepare(
-      "DELETE FROM todos WHERE id = ? AND user_id = ?"
-    );
-    deleteTodo.run(todoId, userId);
+    await prismaDb.todo.delete({
+      where: {
+        id: todoId,
+        userId,
+      },
+    });
     res.sendStatus(200);
   } catch (error) {
     console.log(error.message);
